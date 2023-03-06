954a2c0645f371315702456bf754c86a1a90ccad
benjaminmiharris <benjmiharris@gmail.com>
2023-03-06 12:51:46 +0200

    Project set up

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.post("/test", (req, res) => {
+  console.log("Server is working");
+
+  return res.status(200).send({
+    success: true,
+    message: "successful",
+  });
+});
+
+app.listen(3002, async () => {
+  console.log("Server is running on port 3002");
+});
