322d8063c0cee712003c71946d400b970b4780e4
benjaminmiharris <benjmiharris@gmail.com>
2023-03-14 09:26:09 +0200

    Get properties API set up - need to apply search functionality

--- controllers/PropertiesController.js
+++ controllers/PropertiesController.js
@@ -13,7 +13,7 @@ module.exports = class PropertiesController {
     } catch (error) {
       return res.status(400).json({
         success: false,
-        message: error,
+        message: `Error retrieving property search data - ${error}`,
       });
     }
   };
